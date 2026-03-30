5fa400d169fd40e3091d9042de0b0cf009caffac
VT2026milimet <vinhtranbkdn@gmail.com>
2026-03-30 23:07:21 +0700

    .

---
+++ chemicals.js
@@ -0,0 +1,37 @@
+﻿const chemicalDataRaw = `
+Acid | Acetic acid 10% | CH3COOH.jpg | CH3COOH | lỏng
+Acid | Hydrochloric acid | HCl.jpg | HCl | lỏng
+Acid | Sulfuric acid | H2SO4.jpg | H2SO4 | lỏng
+Acid | Nitric acid | HNO3.jpg | HNO3 | lỏng
+Base | Sodium hydroxide | NaOH.jpg | NaOH | lỏng
+Base | Barium hydroxide | Ba(OH)2.jpg | Ba(OH)2 | lỏng
+Base | Ammonium hydroxide | NH4OH.jpg | NH4OH | lỏng
+Base | Calcium hydroxide | Ca(OH)2.jpg | Ca(OH)2 | lỏng
+Base | Potassium hydroxide | KOH.jpg | KOH | lỏng
+Chất chỉ thị | Metyl da cam | Metyldacam.jpg | Metyl da cam | lỏng
+Chất chỉ thị | Phenolphtalein | Phenolphtalein.jpg | Phenolphtalein | lỏng
+Chất chỉ thị | Quỳ tím | Quytim.jpg | Quỳ tím | lỏng
+Chất oxi hóa | Hydrogen peoxide | H2O2.jpg | H2O2 | lỏng
+Chất oxi hóa | Potassium dichromate | K2Cr2O7.jpg | K2Cr2O7 | lỏng
+Chất oxi hóa | Potassium permanganate | KMnO4.jpg | KMnO4 | lỏng
+Dung môi | Ammonia 5% | NH3.jpg | NH3 | lỏng
+Dung môi | Acetone | (CH3)2CO.jpg | (CH3)2CO | lỏng
+Dung môi | Ethanol | C2H5OH.jpg | C2H5OH | lỏng
+Dung môi | Nước cất | H2O.jpg | H2O | lỏng
+Hợp chất hữu cơ | Glucose 1% | Glucose.jpg | C6H12O6 | lỏng
+Hợp chất hữu cơ | Hồ tinh bột | Hotinhbot.jpg | Hồ tinh bột | lỏng
+Kim loại | Zinc (kẽm) | Zn.jpg | Zn | rắn
+Kim loại | Đinh sắt (Iron) | Dinhsat.jpg | Fe | rắn
+Kim loại | Copper (Đồng) | Cu.jpg | Cu | rắn
+Kim loại | Aluminium (Nhôm) | Al.jpg | Al | rắn
+Kim loại | Iron (Sắt) | Fe.jpg | Fe | rắn
+Muối | Copper(II)sulfate | CuSO4.jpg | CuSO4 | lỏng
+Muối | Silver nitrate 1% | AgNO3.jpg | AgNO3 | lỏng
+Muối | Barium chloride | BaCl2.jpg | BaCl2 | lỏng
+Muối | Calcium carbonate | CaCO3.jpg | CaCO3 | rắn
+Muối | Sodium chloride | NaCl.jpg | NaCl | lỏng
+Muối | Sodium sulfate | Na2SO4.jpg | Na2SO4 | lỏng
+Phi kim | Carbon | C.jpg | C | rắn
+Phi kim | Iodine | I2.jpg | I2 | lỏng
+Phi kim | Sulfur | S.jpg | S | rắn
+`;
